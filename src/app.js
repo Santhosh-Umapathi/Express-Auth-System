6a215f3da9ed32c142549ce63e718921058f3d40
Santhosh Umapathi <Santhosh.Professional1@gmail.com>
2021-11-19 06:46:12 +0100

    FEAT: Login/registration completed

--- src/app.js
+++ src/app.js
@@ -53,6 +53,7 @@ app.post("/register", async (req, res, next) => {
       );
 
       user.token = token;
+      user.password = undefined;
       console.log("🚀 --- app.post --- user", user);
 
       return res.status(201).json(user);
@@ -62,6 +63,50 @@ app.post("/register", async (req, res, next) => {
   } catch (error) {
     console.log("🚀 --- app.post --- error", error);
     return res.status(400).send("Error Occured");
+  }
+});
+
+app.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (email !== "" && password !== "") {
+      const existingUser = await User.findOne({ email });
+
+      if (!existingUser) {
+        res.status(401).send("Not registered");
+      }
+
+      const isPasswordValid = await bcryptjs.compare(
+        password,
+        existingUser.password
+      );
+
+      if (!isPasswordValid) {
+        res.status(401).send("Incorrect Password");
+      }
+
+      const token = jwt.sign(
+        {
+          user_id: existingUser._id,
+          email,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: 3600,
+        }
+      );
+
+      existingUser.token = token;
+      existingUser.password = undefined;
+
+      res.status(200).json(existingUser);
+    } else {
+      res.status(400).send("All fields required");
+    }
+  } catch (error) {
+    console.log("🚀 --- app.post --- error", error);
+    res.status(400).send("Error Occured");
   }
 });
 
